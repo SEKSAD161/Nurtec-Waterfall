e0e4c295168f6fec3f34446b94c24601d48199f7
SEKSAD161 <Devang.Seksaria@pfizer.com>
2026-08-06 19:13:51 +0530

    QoQ Metrics: exclude 2026Q3 (partial quarter, incomplete data)

--- streamlit_app.py
+++ streamlit_app.py
@@ -816,10 +816,13 @@ with tab_qoq:
         )
 
         metric_code = _METRIC_OPTIONS[metric_label]
+        # Drop quarters known to be partial / incomplete for QoQ trending.
+        _INCOMPLETE_QTRS = {"2026Q3"}
+        qoq_df = laad[~laad["QTR"].isin(_INCOMPLETE_QTRS)]
         if selected_payers:
             st.altair_chart(
                 qoq_metric_chart(
-                    laad,
+                    qoq_df,
                     metric=metric_code,
                     brand=brand,
                     claim_type=claim_type,
